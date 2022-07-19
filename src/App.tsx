18a0385f5884bd82215e70311916acadcbd0d605
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-07-18 22:03:08 -0300

    feat: New Lines on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
 
 	//const a = 12
-	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfasdf
+	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfasdf sadfasdfsd
 
 	return (<h1>channel-mixer</h1>)
 }
