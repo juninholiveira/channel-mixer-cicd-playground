c7734458005ed77837442fa130c95e0ef5aa68cf
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-07-19 23:16:41 -0300

    feat: New Cool Feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
 
 	//const a = 12
-	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfasdf sadfasdfsd sadfasdf asdfasdf sdafsadf sdfgdsfg
-	//asdfasdfsdffsddfghfdgh dsfgsdfgdsf asdfasdfsd asdfasdfasfd sdfsdf dfgsdgsdfg sdfsdfsdfsdf
+	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfa sdf sadfasdfsd sadfasdf asdfasdf sdafsadf sdfgdsfg
+	//asdfasdfsdffsddfghfdgh dsfgsdfgdsf asdfasdfsd asdfasdfasfd sdfsdf dfgsdgsdfg sdfsdfsdfsdf asda sda sd
 
 	return (<h1>channel-mixer</h1>)
 }
