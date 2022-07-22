5226f5b44d679ad46d34ec167c9378b75c9b7303
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-07-22 00:00:59 -0300

    fix: teste 3 (#14)
    * Update App.tsx
    
    * update 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
 	//const a = 12
 	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfa sdf sadfasdfsd sadfasdf asdfasdf sdafsadf sdfgdsfg
 	//asdfasdfsdffsddfghfdgh dsfgsdfgdsf asdfasdfsd asdfasdfasfd sdfsdf dfgsdgsdfg sdfsdfsdfsdf asda sda sd asdasd asd sfd sdfsfd asdfsasafd asdfasdf
-	// asdasdasd
+	// asdasdasd asdasd asd asdasd asd
 
 	return (<h1>channel-mixer</h1>)
 }
