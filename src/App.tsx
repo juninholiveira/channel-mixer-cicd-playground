766cbd30bccbfdcf2b8443d1af0966da7603c62c
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-07-22 22:43:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,6 @@ export default function App() {
 	// sdfgsdfgsdfg sdfgsdf g dfgdfg sdaasdf sdfgdsfg dfgdfg sdfgsdfgdfg sdfgsdfgsdfg sadfasdf sdfa sdf sadfasdfsd sadfasdf asdfasdf sdafsadf sdfgdsfg
 	//asdfasdfsdffsddfghfdgh dsfgsdfgdsf asdfasdfsd asdfasdfasfd sdfsdf dfgsdgsdfg sdfsdfsdfsdf asda sda sd asdasd asd sfd sdfsfd asdfsasafd asdfasdf
 	// asdasdasd asdasd asd asdasd asd asda sd asd as da sd sdf sdfsd sdfsdfsdf sdfsdfsdf asa sda ssas asd asda s sdfsdfsdf sdfasdf
+	// asdasdasd
 	return (<h1>channel-mixer</h1>)
 }
